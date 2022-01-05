d964009441cec3f75d0bbeddb08df7f306578440
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2022-01-05 17:39:07 +0000

    Add default device name

--- protonvpn_connection/vpnconfig/abstract_vpnconfig.py
+++ protonvpn_connection/vpnconfig/abstract_vpnconfig.py
@@ -17,6 +17,14 @@ class AbstractVPNCredentials(ABC):
 class AbstractVPNConfiguration(ABC):
 
     @property
+    def default_device_name(self) -> str:
+        return "proton0"
+
+    @property
+    def device_name(self) -> str:
+        raise NotImplementedError
+
+    @property
     @abstractmethod
     def servername(self) -> str:
         raise NotImplementedError

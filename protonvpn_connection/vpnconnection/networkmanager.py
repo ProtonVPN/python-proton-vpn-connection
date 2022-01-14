3f55f5cb43fcf448a26f0bb5d7edecadca739755
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2022-01-14 17:59:48 +0000

    Add and connect to openvpn via nm

--- protonvpn_connection/vpnconnection/networkmanager.py
+++ protonvpn_connection/vpnconnection/networkmanager.py
@@ -1,7 +1,12 @@
 from .vpnconnection import VPNConnection
 
+import gi
+gi.require_version("NM", "1.0")
+from gi.repository import NM
+from .nmclient import NMClientMixin
 
-class NMConnection(VPNConnection):
+
+class NMConnection(VPNConnection, NMClientMixin):
     """Returns VPN connections based on Network Manager implementation.
 
     This is the default backend that will be returned. See docstring for
@@ -35,8 +40,64 @@ class NMConnection(VPNConnection):
     def down(self):
         raise NotImplementedError
 
+    def _import_vpn_config(self, vpnconfig):
+        plugin_info = NM.VpnPluginInfo
+        vpn_plugin_list = plugin_info.list_load()
+
+        with vpnconfig as filename:
+            for plugin in vpn_plugin_list:
+                plugin_editor = plugin.load_editor_plugin()
+                # return a NM.SimpleConnection (NM.Connection)
+                # https://lazka.github.io/pgi-docs/NM-1.0/classes/SimpleConnection.html
+                try:
+                    connection = plugin_editor.import_(filename)
+                    # plugin_name = plugin.props.name
+                except gi.repository.GLib.Error:
+                    pass
+
+        if connection is None:
+            raise NotImplementedError(
+                "Support for given configuration is not implemented"
+            )
+
+        # https://lazka.github.io/pgi-docs/NM-1.0/classes/Connection.html#NM.Connection.normalize
+        if connection.normalize():
+            print("Connection normalized")
+
+        return connection
+
+    def _get_protonvpn_connection(self):
+        """Get ProtonVPN connection.
+
+        Returns:
+            if:
+            - NetworkManagerConnectionTypeEnum.ALL: NM.RemoteConnection
+            - NetworkManagerConnectionTypeEnum.ACTIVE: NM.ActiveConnection
+        """
+        protonvpn_connection = False
+
+        for conn in self.nm_client.get_connections():
+            if conn.get_connection_type() == "vpn":
+                conn_for_vpn = conn
+
+                try:
+                    vpn_settings = conn_for_vpn.get_setting_vpn()
+                except AttributeError:
+                    continue
+
+                if (
+                    vpn_settings.get_data_item("dev")
+                    == self.virtual_device_name
+                ):
+                    protonvpn_connection = conn
+                    break
+
+        return protonvpn_connection
+
 
 class OpenVPN(NMConnection):
+    virtual_device_name = "proton0"
+    connection = None
 
     @staticmethod
     def get_by_protocol(protocol: str):
@@ -45,6 +106,90 @@ class OpenVPN(NMConnection):
             return OpenVPNTCP
         else:
             return OpenVPNUDP
+
+    def _configure_connection(self, vpnconfig, cert_based=False):
+        self.connection = self._import_vpn_config(vpnconfig)
+
+        self.vpn_settings = self.connection.get_setting_vpn()
+        self.connection_settings = self.connection.get_setting_connection()
+
+        self.make_vpn_user_owned()
+        self.add_server_certificate_check()
+        self.dns_configurator()
+        self.set_custom_connection_id()
+        self.apply_virtual_device_type()
+
+        if not cert_based:
+            self.add_vpn_credentials()
+
+    def make_vpn_user_owned(self):
+        # returns NM.SettingConnection
+        # https://lazka.github.io/pgi-docs/NM-1.0/classes/SettingConnection.html#NM.SettingConnection
+        from getpass import getuser
+
+        self.connection_settings.add_permission(
+            "user",
+            getuser(),
+            None
+        )
+
+    def add_server_certificate_check(self):
+        appened_domain = "name:" + self._vpnserver.domain
+        self.vpn_settings.add_data_item(
+            "verify-x509-name", appened_domain
+        )
+
+    def dns_configurator(self):
+        """Apply dns configurations to ProtonVPN connection."""
+
+        ipv4_config = self.connection.get_setting_ip4_config()
+        ipv6_config = self.connection.get_setting_ip6_config()
+
+        ipv4_config.props.dns_priority = -1500
+        ipv6_config.props.dns_priority = -1500
+
+        try:
+            if len(self._settings.dns_custom_ips) == 0:
+                return
+        except AttributeError:
+            return
+
+        custom_dns = self.settings.dns_custom_ips
+        ipv4_config.props.ignore_auto_dns = True
+        ipv6_config.props.ignore_auto_dns = True
+
+        ipv4_config.props.dns = custom_dns
+
+    def set_custom_connection_id(self):
+        try:
+            self.connection_settings.props.id = "ProtonVPN {}".foramt(
+                self._vpnserver.servername if self._vpnserver.servername else "Connection")
+        except AttributeError:
+            self.connection_settings.props.id = "ProtonVPN Connection"
+
+    def apply_virtual_device_type(self):
+        """Apply virtual device type and name."""
+        # Changes virtual tunnel name
+        self.vpn_settings.add_data_item("dev", self.virtual_device_name)
+        self.vpn_settings.add_data_item("dev-type", "tun")
+
+    def add_vpn_credentials(self):
+        """Add OpenVPN credentials to ProtonVPN connection.
+
+        Args:
+            openvpn_username (string): openvpn/ikev2 username
+            openvpn_password (string): openvpn/ikev2 password
+        """
+        # returns NM.SettingVpn if the connection contains one, otherwise None
+        # https://lazka.github.io/pgi-docs/NM-1.0/classes/SettingVpn.html
+        user_data = self._vpnaccount.get_username_and_password()
+
+        self.vpn_settings.add_data_item(
+            "username", user_data.username
+        )
+        self.vpn_settings.add_secret(
+            "password", user_data.password
+        )
 
 
 class OpenVPNTCP(OpenVPN):
@@ -55,14 +200,12 @@ class OpenVPNTCP(OpenVPN):
         from ..vpnconfiguration import OVPNFileConfig
         vpnconfig = OVPNFileConfig(self._vpnserver, self._vpnaccount, self._settings)
         vpnconfig.protocol = self.protocol
-        with vpnconfig as f:
-            print(f)
-            # import with pluging tool
-            # add connection to NM
+        self._configure_connection(vpnconfig)
+        self._add_connection_async(self.connection)
 
     def up(self):
         self._setup()
-        # start connection
+        self._start_connection_async(self._get_protonvpn_connection())
 
     def down(self):
         pass
